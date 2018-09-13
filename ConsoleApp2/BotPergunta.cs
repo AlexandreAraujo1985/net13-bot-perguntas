ee2fdd37b0bc40d3210dfd061a451bf7fe41089b
Fabricio Catae <fcatae@microsoft.com>
2018-09-12 22:20:03 -0300

    primeiros passos

---
+++ ConsoleApp2/BotPergunta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using StackExchange.Redis;
+
+namespace ConsoleApp2
+{
+    class BotPergunta
+    {
+        const string CHANNEL_PERGUNTAS = "perguntas";
+        private readonly IDatabase _db;
+        private readonly ISubscriber _pub;
+
+        public BotPergunta(string connectionString)
+        {
+            var client = ConnectionMultiplexer.Connect(connectionString);
+            var db = client.GetDatabase();
+            var pub = client.GetSubscriber();
+
+            this._db = db;
+            this._pub = pub;
+        }
+
+        public void Perguntar(int id, string texto)
+        {
+            string pergunta = $"P{id}: {texto}";
+            _pub.Publish(CHANNEL_PERGUNTAS, pergunta);
+        }
+
+        public IEnumerable<Resposta> LerRespostas(int id)
+        {
+            string perguntaId = "P" + id.ToString();
+
+            var respostas = _db.HashGetAll(perguntaId)
+                                .Select(entry => new Resposta
+                                {
+                                    Autor = entry.Name,
+                                    Texto = entry.Value
+                                })
+                                .ToList();
+
+            return respostas;
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,8 +5,10 @@ namespace ConsoleApp2
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {            
+            Console.WriteLine("Perguntas");
+
+            string connectionString = (args.Length > 0) ? args[0] : "localhost";
         }
     }
 }
